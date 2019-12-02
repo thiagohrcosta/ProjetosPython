6d4720e16239134ca20062df3675e6fb44a7a61e
thiagohrcosta <thiago_henrique86@hotmail.com>
2019-12-02 18:29:11 -0300

    Create calculadora.py
    Criando pequeno código para elaboração de cálculos matemáticos simples.

---
+++ Calculadora/calculadora.py
@@ -0,0 +1,31 @@
+"""
+Código básico para elaboração de cálculos matemáticos simples.
+"""
+
+while True:
+    print()
+    num_1 = input("Digite um número qualquer:")
+    num_2 = input("Digite outro número qualquer:")
+
+    if not num_1.isnumeric() or not num_2.isnumeric():
+        print("Você precisa digitar um número válido.")
+        continue
+
+    operador = input("Digite o tipo de operador (-, +, *, / ou %):")
+
+    num_1 = int(num_1)
+    num_2 = int(num_2)
+
+    if operador == '+':
+        print(num_1 + num_2)
+    elif operador == '-':
+        print(num_1 - num_2)
+    elif operador == '%':
+        print(num_1 % num_2)
+    elif operador == '*':
+        print(num_1 * num_2)
+    else:
+        print(num_1 / num_2)
+
+    print("FIM")
+    break
